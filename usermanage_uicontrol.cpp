1364d40c3b0ac3bf76856f29cdf2657b322320bc
yt2014 <taoy_2007@126.com>
2015-03-30 15:30:22 +0800

    add usermanage_uicontrol.h

---
+++ usermanage_uicontrol.cpp
@@ -0,0 +1,43 @@
+#include "usermanage_uicontrol.h"
+
+
+CUserManage_UIControl::CUserManage_UIControl()
+{
+
+}
+
+CUserManage_UIControl::CUserManage_UIControl(CAccountTable * ptr_accountTable)
+    :m_accountTable(ptr_accountTable),m_currentIndex(-1)
+{
+     m_currentRecord = new  OneUserInfo();
+     m_currentRecord->name = "";
+     m_currentRecord->password = "";
+     m_currentRecord->permission = "";
+}
+
+bool CUserManage_UIControl::set_Status(Operation_Status current_op_status)
+{
+   m_Current_Op_Status = current_op_status;
+   return true;
+}
+
+bool CUserManage_UIControl::set_index(int index)
+{
+    m_currentIndex = index;
+    return true;
+}
+
+bool CUserManage_UIControl::set_CurrentRecord(OneUserInfo * currentRecord)
+{
+    m_currentRecord->name = currentRecord->name;
+    m_currentRecord->password = currentRecord->password;
+    m_currentRecord->permission = currentRecord->permission;
+    return true;
+}
+
+
+CUserManage_UIControl::~CUserManage_UIControl()
+{
+    delete m_currentRecord;
+}
+
---
+++ usermanage_uicontrol.h
@@ -0,0 +1,37 @@
+#ifndef USERMANAGE_UICONTROL_H
+#define USERMANAGE_UICONTROL_H
+
+#include "AccountTable.h"
+
+
+
+typedef enum
+{
+    Op_Idle,
+    Op_Add,
+    Op_Update,
+    Op_Delete,
+    Op_Query
+}Operation_Status;
+
+
+class CUserManage_UIControl
+{
+private:
+    Operation_Status m_Current_Op_Status;
+    CAccountTable * m_accountTable;
+    int m_currentIndex;
+    OneUserInfo * m_currentRecord;
+public:
+    bool set_Status(Operation_Status current_op_status);
+    bool set_index(int index);
+    bool set_CurrentRecord(OneUserInfo * currentRecord);
+    CUserManage_UIControl();
+    CUserManage_UIControl(CAccountTable * ptr_accountTable);
+
+    virtual ~CUserManage_UIControl();
+
+};
+
+#endif // USERMANAGE_UICONTROL_H
+
